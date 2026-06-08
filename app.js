defbc0cab51c92e26fa1f17a945438e68f2639f2
Mordorfallen <130564127+Andrealbino23@users.noreply.github.com>
2026-06-07 23:49:21 -0300

    commit

---
+++ app.js
@@ -0,0 +1,87 @@
+// const (nao consigo alterar a variavel , eo mais usado )
+// let (consigo alterar a variavel , aterar para let quando precisar atribuir um novo valor para  a variavel)
+
+
+
+
+
+// strings = textos usar aspas simples e melhor mais a crase (``) e melhor ainda 
+
+// const texto = `andre`
+
+// null quando vc decide nao colocar uma informacao, colocar sempre quando o usuario nao tiver uma informacao como por exemplo estado civil
+
+// undifided quando vc esquece de colocar informacoes 
+
+
+
+
+//  const idade = 18
+
+//  if(idade >= 20){
+
+//     console.log(`maior de idade `);
+    
+//  }
+//  else {
+//     console.log(`nao e maior de idade`);
+    
+//  }
+
+
+// const saldo = 99
+
+// if(saldo >= 100){
+
+//     console.log(`seu saldo e positivo`);
+    
+// }
+// else{
+//     console.log(`seu saldo e negativo`);
+    
+// }
+
+
+// const valor = -99
+
+// if(valor < - 98){
+//     console.log(`seu saldo e negativo `);
+   
+// }
+// else{
+//     console.log(`seu saldo e positivo`);
+
+    
+// }
+
+// const guilherme = `novo`
+
+// if(guilherme == `gay`){
+//     console.log(`guilerme e novo`);
+    
+// }
+// else{
+//     console.log(`guilerme nao e novo`);
+    
+// }
+
+// const davifuma = `vaper` nao esquecer de colocar as `` pq se nao colocar nao funciona 
+
+// if(davifuma == `vaper`){
+//     console.log(`davi fuma vaper`);
+    
+// }
+// else{
+//     console.log(`davi nao fuma vaper`);
+    
+// }
+
+// const andre = 20
+// if(andre <= 20 ){
+//     console.log(`idade correta `);
+    
+// }
+// else{
+//     console.log(`idade errada`);
+    
+// }
